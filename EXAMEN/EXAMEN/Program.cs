db8920dc2044da7690c99e91a82c93b9ad5fadee
Sportsy1 <posadadavid344@gmail.com>
2021-11-17 21:01:55 -0500

    INICIA EL PROYECTO

---
+++ EXAMEN/EXAMEN/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXAMEN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
